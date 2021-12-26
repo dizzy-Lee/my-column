944328ddca0c7479c2ca575de6c719f0ed3d6aa0
dizzyLee <dizzyLee@126.com>
2021-12-26 11:45:51 +0800

    add ColumnList component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,47 @@
 <template>
-  <h1>my-column</h1>
+  <column-list :list="list"></column-list>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue"
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { defineComponent } from "vue";
+import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
+const ColumnListTestData: ColumnProps[] = [
+  {
+    id: 0o1,
+    title: "my column 1",
+    description: "this is a description",
+    avatar: "https://img2.baidu.com/it/u=1520747491,3988420976&fm=26&fmt=auto",
+  },
+  {
+    id: 0o2,
+    title: "my column 2",
+    description: "this is a description",
+    avatar: "https://img2.baidu.com/it/u=1520747491,3988420976&fm=26&fmt=auto",
+  },
+  {
+    id: 0o3,
+    title: "my column 3",
+    description: "this is a description",
+  },
+  {
+    id: 0o2,
+    title: "my column 2",
+    description: "this is a description",
+    avatar: "https://img2.baidu.com/it/u=1520747491,3988420976&fm=26&fmt=auto",
+  },
+];
 export default defineComponent({
   name: "App",
-  components: {},
-})
+  components: {
+    ColumnList,
+  },
+  setup() {
+    return {
+      list: ColumnListTestData,
+    };
+  },
+});
 </script>
 
-<style>
-#app {
-  text-align: center;
-}
-</style>
+<style></style>
